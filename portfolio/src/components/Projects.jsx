f37fc8524a0b2cf796f0bd0bc0850dfe7db0086f
muzzu <muzzueducation@gmail.com>
2023-12-10 17:31:12 +0530

    project sections is now responsive

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -48,29 +48,29 @@ export default function Projects() {
             <SectionTitle title="Recent Works" subTitle="My Projects" para="Lorem ipsum dolor sit amet consectetur. Fames urna nisl purus arcu in turpis convallis." />
             {/* body */}
 
-            <div className='grid grid-cols-12  mt-20' >
+            <div className='grid grid-cols-12 gap-y-[60px] md:gap-y-0 mt-20' >
                 {/* menu  */}
-                <ul className='col-span-5 space-y-5 ' >
+                <ul className='col-span-12 gap-x-5 md:gap-x-0 overflow-scroll  flex flex-row md:flex-col md:col-span-5 md:space-y-5' >
                     {
-                        uniqueCategories.map(category => <li className='opacity-50 font-bold text-lg hover:opacity-80 cursor-pointer transition duration-150 ease-in ' >{category}</li>)
+                        uniqueCategories.map(category => <li className=' px-7 md:px-0 py-[5px] bg-textBlack md:bg-transparent text-white md:text-textBlack capitalize text-sm md:text-lg md:py-0  whitespace-nowrap font-medium rounded-full md:opacity-50 md:hover:opacity-80 cursor-pointer transition duration-150 ease-in ' >{category}</li>)
                     }
                 </ul>
                 {/* projects */}
-                <div className='col-span-6  space-y-11'>
+                <div className='col-span-12 md:col-span-6  space-y-12'>
                     {/* single project */}
 
                     {projectsData.map(({ projectImg, title, subtitle, tags, projectUrl }) => (
                         <div>
-                            <img src={projectImg} className='h-[300px] w-full rounded-2xl object-cover' alt="" />
+                            <img src={projectImg} className='h-[220px] sm:h-[260px] w-full rounded-xl md:rounded-2xl object-cover' alt="" />
 
                             {/* projectInfo  */}
-                            <div className='flex items-start justify-between mt-8 px-1' >
+                            <div className='flex flex-col sm:flex-row items-start justify-between mt-5  sm:mt-8 gap-y-4 md:gap-y-4 px-1' >
                                 <div className='space-y-2'>
                                     {/* project name */}
-                                    <p className='font-extrabold flex gap-x-2 opacity-90 text-2xl' >
+                                    <p className='font-extrabold flex flex-wrap flex-col md:flex-row gap-y-[2px] md:gap-x-2 opacity-90 text-2xl' >
                                         <span>{title}</span>
-                                        <span>-</span>
-                                        <span>{subtitle.length>15?subtitle.slice(0,20)+"...":subtitle}</span>
+                                        <span className='hidden md:block' >-</span>
+                                        <span className='text-xl md:text-2xl opacity-80 md:opacity-100 font-semibold md:font-extrabold ' >{subtitle}</span>
                                     </p>
                                     <p className='opacity-70' >
                                         {tags.join(' ꞏ ')}
@@ -78,12 +78,10 @@ export default function Projects() {
                                 </div>
                                 {/* userActions */}
                                 <div className='flex gap-8' >
-                                    <img src={githubOutlined} className='h-6 w-6 object-contain opacity-50 hover:opacity-80 transition easy-in cursor-pointer' alt="githubOutlined" />
-                                    <img src={arrowCross} className='h-6 w-6 object-contain opacity-50 hover:opacity-80 transition easy-in cursor-pointer' alt="arrowCross" />
+                                    <img src={githubOutlined} className='h-5 w-5 md:h-6 md:w-6 object-contain opacity-50 hover:opacity-80 transition easy-in cursor-pointer' alt="githubOutlined" />
+                                    <img src={arrowCross} className='h-5 w-5 md:h-6 md:w-6 object-contain opacity-50 hover:opacity-80 transition easy-in cursor-pointer' alt="arrowCross" />
                                 </div>
                             </div>
-
-
                         </div>
                     ))}
 
