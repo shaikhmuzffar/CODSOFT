ea2ea57fdda81d4de3d0fa5a1cb2df8eee03757a
muzzu <muzzueducation@gmail.com>
2023-12-10 09:22:53 +0530

    navbar is responsive

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -18,27 +18,33 @@ function App() {
 
   return (
     <>
-      <section className='w-10/12 mx-auto ' >
+      <section className='bg-myPurple md:bg-white ' >
         <Navbar />
-        <Hero />
-        <Partners />
-      </section>
-      <section className='bg-[#F4F5F6] mt-[120px] bg-opacity-50' >
-        <About />
       </section>
       <section className='w-10/12 mx-auto ' >
+        <Hero />
+        {/* <Partners /> */}
+      </section>
+    
+      {/* <section className='bg-[#F4F5F6] mt-[120px] bg-opacity-50' >
+        <About />
+      </section> */}
+
+      {/* <section className='w-10/12 mx-auto ' >
         <Services />
         <Projects />
-      </section>
-      <section className='bg-[#F4F5F6] mt-[120px] bg-opacity-50' >
+      </section> */}
+
+      {/* <section className='bg-[#F4F5F6] mt-[120px] bg-opacity-50' >
         <Features />
-      </section>
-      <section className='w-10/12 mx-auto ' >
+      </section> */}
+
+      {/* <section className='w-10/12 mx-auto ' >
         <Blogs/>
         <Contact/>
         <Footer/>
         <Copyright/>
-      </section>
+      </section> */}
 
 
 
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,53 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // images
 import Logo from '../assets/logo.png'
+import LogoWhite from '../assets/LogoWhite.png'
+import hamburger from '../assets/hamburger.png'
+import cross from '../assets/cross.png'
+
+// components
 import Button from '../Ui components/Button'
 
 export default function Navbar() {
 
+    const [hamMenu,setHamMenu] =useState(false) 
     const navlinks =
         [
             {
                 link: "Home",
-                arrow:false,
+                arrow: false,
             },
             {
                 link: "About",
-                arrow:false,
+                arrow: false,
             },
             {
                 link: "Services",
-                arrow:false,
+                arrow: false,
             },
             {
                 link: "Projects",
-                arrow:false,
+                arrow: false,
             },
             {
                 link: "Blogs",
-                arrow:false,
+                arrow: false,
             }
         ]
 
     return (
-        <section className='h-[90px] ' >
-           <div className='flex justify-between items-center  h-full'>
-             {/* logo */}
-             <img className='h-11 w-[170px] object-contain' src={Logo} alt="" />
-            {/* navigation */}
-            <ul className='flex gap-[60px]' >
-                <li className='text-myPurple font-mulish text-sm font-bold cursor-pointer'>Home</li>
-                <li className='text-blackText font-mulish text-sm font-bold opacity-70 hover:text-myPurple hover:opacity-100 transition duration-300 ease-in cursor-pointer '>About</li>
-                <li className='text-blackText font-mulish text-sm font-bold opacity-70 hover:text-myPurple hover:opacity-100 transition duration-300 ease-in cursor-pointer '>Services</li>
-                <li className='text-blackText font-mulish text-sm font-bold opacity-70 hover:text-myPurple hover:opacity-100 transition duration-300 ease-in cursor-pointer '>Projects</li>
-                <li className='text-blackText font-mulish text-sm font-bold opacity-70 hover:text-myPurple hover:opacity-100 transition duration-300 ease-in cursor-pointer '>Blogs</li>
-            </ul>
-            {/* button */}
-           <Button>
-            Contact
-           </Button>
-           </div>
+        <section className='h-[60px] md:h-[90px] w-10/12 mx-auto  ' >
+            <div className=' flex justify-between items-center h-full'>
+                {/* logo */}
+                <img className='hidden md:block md:h-10 md:w-[120px] lg:h-17 lg:w-[170px] object-contain' src={Logo} alt="" />
+                <img className='h-[30px] w-[120px] md:hidden object-contain' src={LogoWhite} alt="" />
+
+                {/* navigation */}
+
+                <ul className={`${hamMenu?"visible":"invisible"} absolute md:visible md:static z-20 md:z-0 md:flex w-10/12 md:w-fit px-7 md:px-0 py-10 md:py-0 flex-col gap-y-5 top-[60px] md:top-0 left-0  bg-myPurple md:bg-transparent  md:flex-row md:gap-[30px] lg:gap-[60px] transition duration-150 ease-in`}>
+                  
+                    {navlinks.map(({ link }) => (
+                        <li className='text-white md:text-textBlack py-2 text-center hover:bg-[#b96eff] bg-opacity-25 rounded-lg   md:bg-transparent font-mulish text-lg md:text-base font-bold cursor-pointer'>{link}</li>
+                    ))}
+                    {/* button */}
+                    <li> <button className='h-10 px-8 md:hidden w-full md:w-fit mt-[30px] bg-white text-myPurple text-base font-semibold rounded-lg' >
+                        Contact
+                    </button></li>
+                </ul>
+                {/* button */}
+                <div className='hidden md:block' >
+                    <Button>
+                        Contact
+                    </Button>
+                </div>
+
+                {/* hamburger icon */}
+                <div className='md:hidden' >
+                {hamMenu?<img className='h-5 w-5 object-contain' src={cross} alt="cross" onClick={()=>setHamMenu(false)} />
+                    :<img className='h-5 w-5 object-contain' src={hamburger} alt="cross" onClick={()=>setHamMenu(true)} />  }
+                    </div>
+            </div>
         </section>
     )
 }
