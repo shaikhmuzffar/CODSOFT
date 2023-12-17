b906a59c4243c3828cca3893c37452368f2dc20a
muzzu <muzzueducation@gmail.com>
2023-12-17 11:40:31 +0530

    some ui updates

--- portfolio/src/components/Services.jsx
+++ portfolio/src/components/Services.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 // images 
 import servicesImg from '../assets/servicesImg.png'
+import servicesMobileImg from '../assets/servicesMobileImg.png'
 
 //components
 import SectionTitle from '../Ui components/SectionTitle4'
@@ -31,7 +32,7 @@ export default function Services() {
         <SectionTitle title="We Provide" para="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt iusto harum sit magnam cupiditate cum deserunt sapiente ratione dignissimos ducimus." subTitle={'My Services'}  ></SectionTitle>
 
         {/* body  */}
-        <div className='grid grid-cols-12 mt-[90px] gap-y-[60px] md:gap-y-[90px] md:mt-16 font-mulish' >
+        <div className='grid grid-cols-12 mt-[90px] gap-y-[60px] sm:gap-y-[90px] md:gap-y-[90px] md:mt-16 font-mulish' >
             {/* services  */}
             <div className='col-span-12 md:col-span-5 ' >
                <div className='space-y-12 md:space-y-16'>
@@ -40,14 +41,15 @@ export default function Services() {
                     <div className='space-y-4'>
                         <p className={`font-bold flex items-center justify-center text-sm w-[45px] h-[28px] rounded-[30px] text-white bg-${service.color}`} >0{index+1}</p>
                         <h1 className='text-xl font-extrabold opacity-90' >{service.title}</h1>
-                        <p  className='w-10/12 leading-7 md:w-9/12 lg:w-10/12 opacity-70' >{service.para}</p>
+                        <p  className='w-10/12 leading-7 md:w-9/12 line-clamp-3 sm:line-clamp-2 lg:w-10/12 opacity-70' >{service.para}</p>
                     </div>
                 ))}
                </div>
             </div>
             {/* video  */}
             <div className='col-span-12  md:col-span-7 ' >
-                <img src={servicesImg} className='h-[350px] sm:h-[400px]  md:h-[600px] w-full object-contain' alt="" />
+                <img src={servicesImg} className='h-[350px] sm:h-[400px] hidden md:block  md:h-[600px] w-full object-contain' alt="" />
+                <img src={servicesMobileImg} className='h-[350px] sm:h-[400px]  md:hidden  md:h-[600px] w-full object-contain' alt="" />
             </div>
             
         </div>
